25421074ed138b6bef31a184f08c0f07c855d4d1
virustotalop <virustotalop@users.noreply.github.com>
2018-08-06 21:36:58 -0700

    Starting gui command

---
+++ src/main/java/me/virustotal/dynamicgui/command/GUICommand.java
@@ -0,0 +1,11 @@
+package me.virustotal.dynamicgui.command;
+
+import me.virustotal.dynamicgui.entity.player.PlayerWrapper;
+
+public class GUICommand {
+
+	public void execute(PlayerWrapper<?> player, String gui)
+	{
+		
+	}
+}
