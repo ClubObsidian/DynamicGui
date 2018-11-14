fcdc9fff0ba8f7e82481c644915cf64941b846fb
virustotalop <virustotalop@gmail.com>
2018-11-13 19:45:35 -0800

    Updated wrappy for hocon fix - 3.3.2

--- src/main/java/com/clubobsidian/dynamicgui/manager/dynamicgui/GuiManager.java
+++ src/main/java/com/clubobsidian/dynamicgui/manager/dynamicgui/GuiManager.java
@@ -171,7 +171,7 @@ public class GuiManager {
 	{
 		File guiFolder = DynamicGui.get().getPlugin().getGuiFolder();
 		
-		Collection<File> ar = FileUtils.listFiles(guiFolder, new String[]{"yml", "json", "hocon"}, true);
+		Collection<File> ar = FileUtils.listFiles(guiFolder, new String[]{"yml", "json", "conf"}, true);
 		
 		if(ar.size() != 0)
 		{
