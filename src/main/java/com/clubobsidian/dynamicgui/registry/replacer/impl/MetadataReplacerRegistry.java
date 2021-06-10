5803e403e41172b42b45668c4311d5c7bc7d5eca
virustotalop <virustotalop@gmail.com>
2021-06-10 01:42:38 -0700

    Revert make metadata actually able to run at gui creation

--- src/main/java/com/clubobsidian/dynamicgui/registry/replacer/impl/MetadataReplacerRegistry.java
+++ src/main/java/com/clubobsidian/dynamicgui/registry/replacer/impl/MetadataReplacerRegistry.java
@@ -15,18 +15,15 @@
  */
 package com.clubobsidian.dynamicgui.registry.replacer.impl;
 
-import com.clubobsidian.dynamicgui.DynamicGui;
 import com.clubobsidian.dynamicgui.entity.PlayerWrapper;
-import com.clubobsidian.dynamicgui.event.inventory.GuiLoadEvent;
-import com.clubobsidian.dynamicgui.event.inventory.GuiPreloadEvent;
-import com.clubobsidian.dynamicgui.event.inventory.InventoryCloseEvent;
 import com.clubobsidian.dynamicgui.gui.Gui;
+import com.clubobsidian.dynamicgui.manager.dynamicgui.GuiManager;
+import com.clubobsidian.dynamicgui.manager.dynamicgui.cooldown.Cooldown;
+import com.clubobsidian.dynamicgui.manager.dynamicgui.cooldown.CooldownManager;
 import com.clubobsidian.dynamicgui.registry.replacer.ReplacerRegistry;
-import com.clubobsidian.trident.EventHandler;
-import com.clubobsidian.trident.EventPriority;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 
@@ -35,7 +32,7 @@ public class MetadataReplacerRegistry implements ReplacerRegistry {
     private static MetadataReplacerRegistry instance;
 
     public static MetadataReplacerRegistry get() {
-        if(instance == null) {
+        if (instance == null) {
             instance = new MetadataReplacerRegistry();
         }
 
@@ -44,51 +41,22 @@ public class MetadataReplacerRegistry implements ReplacerRegistry {
 
     private static final String METADATA_PREFIX = "%metadata_";
 
-    private final Map<UUID, Gui> cachedGuis;
-
-    private MetadataReplacerRegistry() {
-        this.cachedGuis = new HashMap<>();
-        DynamicGui.get().getEventBus().registerEvents(this);
-    }
-
     @Override
     public String replace(PlayerWrapper<?> playerWrapper, String text) {
-        UUID uuid = playerWrapper.getUniqueId();
-        Gui gui = this.cachedGuis.get(uuid);
-        if(gui == null) {
+        Gui gui = GuiManager.get().getCurrentGui(playerWrapper);
+        if (gui == null) {
             return text;
         }
 
-        for(Map.Entry<String, String> entry : gui.getMetadata().entrySet()) {
+        for (Map.Entry<String, String> entry : gui.getMetadata().entrySet()) {
             String metadataKey = entry.getKey();
             String metadataValue = entry.getValue();
             String metadataReplacer = METADATA_PREFIX + metadataKey + "%";
-            if(text.contains(metadataReplacer)) {
+            if (text.contains(metadataReplacer)) {
                 text = StringUtils.replace(text, metadataReplacer, metadataValue);
             }
         }
 
         return text;
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onGuiPreload(GuiPreloadEvent event) {
-        UUID uuid = event.getPlayerWrapper().getUniqueId();
-        Gui gui = event.gui();
-        this.cachedGuis.put(uuid, gui);
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onGuiLoad(GuiLoadEvent event) {
-        if(event.isCancelled()) {
-            UUID uuid = event.getPlayerWrapper().getUniqueId();
-            this.cachedGuis.remove(uuid);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onInventoryClose(InventoryCloseEvent event) {
-        UUID uuid = event.getPlayerWrapper().getUniqueId();
-        this.cachedGuis.remove(uuid);
     }
 }
