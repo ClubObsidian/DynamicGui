7c07a46c31fe735bbd19c1b1e8c2c2fdae0f005f
virustotalop <virustotalop@gmail.com>
2022-03-15 00:27:49 -0700

    Test async functions

--- core/src/test/java/com/clubobsidian/dynamicgui/core/test/manager/FunctionManagerTest.java
+++ core/src/test/java/com/clubobsidian/dynamicgui/core/test/manager/FunctionManagerTest.java
@@ -3,11 +3,9 @@ package com.clubobsidian.dynamicgui.core.test.manager;
 import com.clubobsidian.dynamicgui.core.entity.PlayerWrapper;
 import com.clubobsidian.dynamicgui.core.gui.Gui;
 import com.clubobsidian.dynamicgui.core.gui.Slot;
-import com.clubobsidian.dynamicgui.core.inventory.InventoryWrapper;
 import com.clubobsidian.dynamicgui.core.manager.dynamicgui.FunctionManager;
 import com.clubobsidian.dynamicgui.core.manager.dynamicgui.GuiManager;
-import com.clubobsidian.dynamicgui.core.test.mock.MockFactory;
-import com.clubobsidian.dynamicgui.core.test.mock.entity.player.MockPlayer;
+import com.clubobsidian.dynamicgui.core.test.mock.ScheduledTest;
 import com.clubobsidian.dynamicgui.core.test.mock.entity.player.MockPlayerWrapper;
 import com.clubobsidian.dynamicgui.parser.function.FunctionType;
 import org.junit.jupiter.api.Test;
@@ -18,16 +16,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class FunctionManagerTest {
-
-    private final MockFactory factory = new MockFactory();
+public class FunctionManagerTest implements ScheduledTest {
 
     @Test
     public void testPassing() throws ExecutionException, InterruptedException {
-        this.factory.inject();
-        MockPlayerWrapper playerWrapper = this.factory.createPlayer();
+        MockPlayerWrapper playerWrapper = this.getFactory().createPlayer();
         Gui gui = GuiManager.get().getGui("test");
-        assertTrue(gui.getSlots().size() >= 1);
         Slot slot = gui.getSlots().get(0);
         assertTrue(FunctionManager.get().tryFunctions(slot, FunctionType.CLICK, playerWrapper).get());
         assertTrue(playerWrapper.getIncomingChat().size() == 1);
@@ -36,13 +30,24 @@ public class FunctionManagerTest {
 
     @Test
     public void testFailing() throws ExecutionException, InterruptedException {
-        this.factory.inject();
-        MockPlayerWrapper playerWrapper = this.factory.createPlayer();
+        MockPlayerWrapper playerWrapper = this.getFactory().createPlayer();
         Gui gui = GuiManager.get().getGui("test");
-        assertTrue(gui.getSlots().size() >= 1);
         Slot slot = gui.getSlots().get(1);
         assertFalse(FunctionManager.get().tryFunctions(slot, FunctionType.CLICK, playerWrapper).get());
         assertTrue(playerWrapper.getIncomingChat().size() == 1);
         assertEquals("fail", playerWrapper.getIncomingChat().get(0));
+    }
+
+    @Test
+    public void testAsync() throws ExecutionException, InterruptedException {
+        MockPlayerWrapper playerWrapper = this.getFactory().createPlayer();
+        Gui gui = GuiManager.get().getGui("test");
+        Slot slot = gui.getSlots().get(2);
+        long time = System.currentTimeMillis();
+        assertTrue(FunctionManager.get().tryFunctions(slot, FunctionType.CLICK, playerWrapper).get());
+        assertTrue(playerWrapper.getIncomingChat().size() == 1);
+        assertEquals("async", playerWrapper.getIncomingChat().get(0));
+        long delta = System.currentTimeMillis() - time;
+        assertTrue(delta >= 500);
     }
 }
