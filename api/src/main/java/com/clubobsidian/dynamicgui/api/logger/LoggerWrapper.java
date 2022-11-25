5e96d4fad88962ec59b83dab9f9e60fd2396d65d
virustotalop <virustotalop@gmail.com>
2022-11-25 06:45:37 -0800

    Document logger wrapper

--- api/src/main/java/com/clubobsidian/dynamicgui/api/logger/LoggerWrapper.java
+++ api/src/main/java/com/clubobsidian/dynamicgui/api/logger/LoggerWrapper.java
@@ -24,11 +24,27 @@ public abstract class LoggerWrapper<T> {
         this.logger = logger;
     }
 
+    /**
+     * Gets the native underlying logger object that
+     * this wrapper wraps.
+     *
+     * @return the native logger
+     */
     public T getLogger() {
         return this.logger;
     }
 
+    /**
+     * Logs the given info message
+     *
+     * @param message the message to log
+     */
     public abstract void info(String message);
 
+    /**
+     * Logs the given message error message
+     *
+     * @param message the message to log
+     */
     public abstract void error(String message);
 }
