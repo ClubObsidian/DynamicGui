334bad27e88fa620f7c7829f1d91245c435b84cd
virustotalop <virustotalop@gmail.com>
2025-10-08 21:36:41 -0700

    Bump license year

--- mock/src/main/java/com/clubobsidian/dynamicgui/mock/package-info.java
+++ mock/src/main/java/com/clubobsidian/dynamicgui/mock/package-info.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2018-2023 virustotalop
+ *    Copyright 2018-2025 virustotalop
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
